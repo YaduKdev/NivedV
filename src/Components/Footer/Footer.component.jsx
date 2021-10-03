c09aa5534f607d2d5bcbfa76a02f23ad0bad375d
YaduKdev <yaduk.developer@gmail.com>
2021-10-03 15:30:08 +0530

    changed git url local

--- src/Components/Footer/Footer.component.jsx
+++ src/Components/Footer/Footer.component.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className='footer section'>
         <h1 className='footer-header'>Bye!</h1>
         <img className='footer-image' src={footerImage} alt='footer-sticker' />
-        <h3>TEST</h3>
+        <h3>TEST git config</h3>
     </div>
 );
 
