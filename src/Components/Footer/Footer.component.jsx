6d6efd09db80518965d7b2fd695ee9d498325da7
YaduKdev <yaduk.developer@gmail.com>
2021-10-03 15:32:46 +0530

    successfully changed repository name

--- src/Components/Footer/Footer.component.jsx
+++ src/Components/Footer/Footer.component.jsx
@@ -6,7 +6,6 @@ const Footer = () => (
     <div className='footer section'>
         <h1 className='footer-header'>Bye!</h1>
         <img className='footer-image' src={footerImage} alt='footer-sticker' />
-        <h3>TEST git config</h3>
     </div>
 );
 
